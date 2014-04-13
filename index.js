2b6302602a435b8a6e2e34aa75f5431ca430e735
Marco Loche <marco.loche@capturator.com>
2014-04-13 22:22:47 +0200

    Version 2 : refactoring loop logic and creates a specialized function

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ var getNumber = require('./lib/get-number');
 function convertNumeralToNumber(numeral) {
   var decimal = 0;
   for (var i = 0; i < numeral.length; i++) {
-    if ( numeral[i + 1] && getNumber(numeral[i]) < getNumber(numeral[i + 1]) )
-      decimal -= getNumber(numeral[i]);
-    else
-      decimal += getNumber(numeral[i]);
+    decimal += getDecimal(getNumber(numeral[i]), getNumber(numeral[i + 1]));
   }
   return decimal;
+}
+
+var getDecimal = function (n, nPlusOne) {
+  if (nPlusOne) {
+    return (n < nPlusOne ? -n : n);
+  }
+  return n;
 }
 
 module.exports = convertNumeralToNumber;
