cc1aa5912458e7ee84796a891a0b45918cfa6571
Marco Loche <marco.loche@capturator.com>
2014-04-13 22:21:57 +0200

    Version 1

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 var getNumber = require('./lib/get-number');
 function convertNumeralToNumber(numeral) {
-  // TODO: write the code here!
+  var decimal = 0;
+  for (var i = 0; i < numeral.length; i++) {
+    if ( numeral[i + 1] && getNumber(numeral[i]) < getNumber(numeral[i + 1]) )
+      decimal -= getNumber(numeral[i]);
+    else
+      decimal += getNumber(numeral[i]);
+  }
+  return decimal;
 }
+
 module.exports = convertNumeralToNumber;
 
